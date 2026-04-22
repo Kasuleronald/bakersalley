186f38537e11a1105bb20a44034ca190af2a0eb4
Kasule Ronald <ronald.t.kasule@gmail.com>
2026-04-22 21:00:22 +0300

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -54,9 +54,9 @@ export const DEPARTMENTS: DepartmentName[] = [
 ];
 
 export const INITIAL_USERS: User[] = [
-  { id: 'u-md', name: 'Dr. David Kato', identity: 'md@nissi-industries.com', passwordHash: 'md123', department: 'Administration', role: 'Managing Director', authorityLimit: 1000000000, mfaEnabled: true, hasConsentedToPrivacy: true, seenFeatures: [], systemVersion: '0.0.0' },
-  { id: 'u-ops', name: 'Sarah Nabukeera', identity: 'ops@nissi-industries.com', passwordHash: 'ops123', department: 'Production', role: 'Plant Manager', authorityLimit: 50000000, mfaEnabled: true, hasConsentedToPrivacy: true, seenFeatures: [], systemVersion: '0.0.0' }
-  { id: 'u-dummy', name: 'Dummy Company', identity: 'dummy', passwordHash: 'dummy@123', department: 'Administration', role: 'Managing Director', authorityLimit: 1000000000, mfaEnabled: false, hasConsentedToPrivacy: true, seenFeatures: [], systemVersion: '0.0.0' }
+  { id: 'u-md', name: 'Dr. David Kato', identity: 'md@nissi-industries.com', passwordHash: 'md123', department: 'Administration', role: 'Managing Director', authorityLimit: 1000000000, mfaEnabled: true },
+  { id: 'u-ops', name: 'Sarah Nabukeera', identity: 'ops@nissi-industries.com', passwordHash: 'ops123', department: 'Production', role: 'Plant Manager', authorityLimit: 50000000, mfaEnabled: true },
+  { id: 'u-dummy', name: 'Dummy Company', identity: 'dummy', passwordHash: 'dummy@123', department: 'Administration', role: 'Managing Director', authorityLimit: 1000000000, mfaEnabled: false }
 ];
 
 export const INITIAL_EMPLOYEES: Employee[] = [
