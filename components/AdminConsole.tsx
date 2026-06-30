6df527ab0d7541921654a4d409b91c193eafba5c
kasul29-06 <ronald.t.kasule@gmail.com>
2026-06-30 11:49:27 +0200

    auto-sync: 2026-06-30 09:49:27.054 UTC

--- components/AdminConsole.tsx
+++ components/AdminConsole.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { DepartmentName, Organization, SubscriptionTier, User, UserRole } from '../types';
 
 interface AdminConsoleProps {
